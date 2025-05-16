00751d49240cf37a624f5f9e4fdaad611bd6c871
chanwookK <kan0202@naver.com>
2025-05-16 10:39:33 +0900

    Feat: UTF-8 인코딩 적용

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 // Your First C++ Program
 
 #include <iostream>
+#include <windows.h>
 
 #include "src/DVM/common/DVMContext.h"
 
 int main() {
     std::cout << "Hello World!";
     std::cout << "nunsori testing2";
-
+    SetConsoleOutputCP(CP_UTF8);
     // DVM 에 필요한 객체들의 의존성 주입 및 생성 (조립)
     auto* dvmContext = new DVMContext();
     dvmContext->run();
