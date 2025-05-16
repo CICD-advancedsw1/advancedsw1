3603c9f145e1b4646189a2aa390a4173f1b8581b
chanwookK <kan0202@naver.com>
2025-05-16 10:38:26 +0900

    Feat: Network 연결 시 필요한 DVM 정보 객체 구현

--- src/DVM/common/data/DVMNetworkData.cpp
+++ src/DVM/common/data/DVMNetworkData.cpp
@@ -3,23 +3,64 @@
 //
 
 #include "DVMNetworkData.h"
-DVMNetworkData::DVMNetworkData() {
-  initDataFromFile();
+std::string DVMNetworkData::DVMId;
+std::vector<std::string> DVMNetworkData::DVMIPs;
+int DVMNetworkData::x;
+int DVMNetworkData::y;
+
+void DVMNetworkData::init(const string& filename) {
+  map<string,string> data = DVMNetworkData::initDataFromFile(filename);
+  x = stoi(data.at("x"));
+  y = stoi(data.at("y"));
+  DVMId = data.at("DVMId");
+  DVMIPs = DVMNetworkData::splitIPs(data.at("DVMIPs"));
 }
-DVMNetworkData::~DVMNetworkData() {
+
+map<string, string> DVMNetworkData::initDataFromFile(const string& filename) {
+  ifstream file(filename);
+  map<string, string> config;
+
+  if (!file.is_open()) {
+    cerr << "[Error] Failed to open file: " << filename << endl;
+    return config;
+  }
+
+  string line;
+  while (std::getline(file, line)) {
+    istringstream iss(line);
+    string key, value;
+
+    if (!(iss >> key >> value)) {
+      cerr << "[Warning] Skipping invalid line: " << line << endl;
+      continue;
+    }
+
+    config[key] = value;
+  }
+
+  file.close();
+  return config;
 }
-void DVMNetworkData::initDataFromFile() {
-  //TODO: file로 메타데이터 변경할 수 있도록
+
+vector<string> DVMNetworkData::splitIPs(const string& csv) {
+  vector<string> result;
+  istringstream ss(csv);
+  string token;
+  while (getline(ss, token, ',')) {
+    result.push_back(token);
+  }
+  return result;
 }
+
 std::string DVMNetworkData::getDVMId() {
   return DVMId;
 }
 std::vector<std::string> DVMNetworkData::getDVMIPs() {
   return DVMIPs;
 }
-std::string DVMNetworkData::getX() {
+int DVMNetworkData::getX() {
   return x;
 }
-std::string DVMNetworkData::getY() {
+int DVMNetworkData::getY() {
   return y;
 }
