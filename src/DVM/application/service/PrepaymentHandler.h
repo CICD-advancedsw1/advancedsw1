70f74ceafb6abc2ad323fb55e6e07ca7ab8b521f
chanwookK <kan0202@naver.com>
2025-05-16 10:38:45 +0900

    Feat: PrepaymentHandler 구현

--- src/DVM/application/service/PrepaymentHandler.h
+++ src/DVM/application/service/PrepaymentHandler.h
@@ -5,22 +5,31 @@
 #ifndef PREPAYMENTHANDLER_H
 #define PREPAYMENTHANDLER_H
 #include <string>
-#include <vector>
 
 #include "../../infrastructure/BroadCast.h"
 
 
+class ResponseStock;
 class Beverage;
 class PrepaymentHandler {
   private:
     BroadCast* broadCast;
-    std::vector<std::pair<std::string, int>> Cert_code;
+    double findDistance(int x, int y);
+    std::string generateCertificationCode(int length);
+
   public:
     PrepaymentHandler(BroadCast* broadCast);
     ~PrepaymentHandler();
-    std::string findAvailableDVM(Beverage* beverage, int qty);
-    void EraseCode(std::string code);
-    int PrePaymentCheck(std::string code);
+    std::string makeRequestStockMessage(int code, int qty);
+    std::string makeRequestPrepaymentMessage(std::string certCode,
+                                             int itemCode,
+                                             int qty,
+                                             std::string dstId);
+    std::pair<bool, std::string> prepaymentRequest(Beverage *beverage,
+                                                   int qty,
+                                                   std::string dstIp,
+                                                   std::string dstId);
+    ResponseStock* findAvailableDVM(Beverage* beverage, int qty);
     //stockCode();
 };
 
